78e2fde861425e7ce76a758a75328f485b9a0d41
Wasim <wasimu285@gmail.com>
2025-03-29 07:09:38 +0000

    Fixed login and signup navigation issue

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import App from './App';
+import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AuthProvider } from "./context/Authcontext"; 
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/">
+      <AuthProvider>  
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
