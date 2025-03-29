167ea24f5ff8131872b232ef51b4514e26a8ccf7
Wasim <wasimu285@gmail.com>
2025-03-29 08:12:58 +0000

    Fixed login and signup navigation issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,72 +11,72 @@ import HomePage from './Pages/HomePage';
 import Dashboard from './Pages/Dashboard';
 import Profile from './Pages/ProfilePage';
 import Pathways from './Pages/PathwaysPage';
-import Lesson from './Pages/LessonPage';sonPage';
+import Lesson from './Pages/LessonPage';
 
 // Admin pages
-import AdminDashboard from './Pages/AdminDashboard';import AdminDashboard from './Pages/AdminDashboard';
-import AdminUsers from './Pages/AdminUsers';ers from './Pages/AdminUsers';
-import AdminUserDetail from './Pages/AdminUserDetail';';
-import AdminUserForm from './Pages/AdminUserForm';Form';
+import AdminDashboard from './Pages/AdminDashboard';
+import AdminUsers from './Pages/AdminUsers';
+import AdminUserDetail from './Pages/AdminUserDetail';
+import AdminUserForm from './Pages/AdminUserForm';
 import AdminPathways from './Pages/AdminPathways';
-import AdminPathwayDetail from './Pages/AdminPathwayDetail';ayDetail';
-import AdminPathwayForm from './Pages/AdminPathwayForm';Form';
+import AdminPathwayDetail from './Pages/AdminPathwayDetail';
+import AdminPathwayForm from './Pages/AdminPathwayForm';
 import AdminLessons from './Pages/AdminLessons';
-import AdminLessonDetail from './Pages/AdminLessonDetail';';
-import AdminLessonForm from './Pages/AdminLessonForm';Form';
+import AdminLessonDetail from './Pages/AdminLessonDetail';
+import AdminLessonForm from './Pages/AdminLessonForm';
 
 import './App.css';
 
 function App() {
-  return (  return (
-    <AuthProvider>r>
-      <Router>ter>
+  return (
+    <AuthProvider>
+      <Router>
         <Routes>
-          {/* Public routes (with regular layout) */}Public routes (with regular layout) */}
-          <Route element={<Layout />}> element={<Layout />}>
+          {/* Public routes (with regular layout) */}
+          <Route element={<Layout />}>
             <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />nt={<LoginPage />} />
-            {/* Register path removed since you already have a signup page */}dy have a signup page */}
+            <Route path="/login" element={<LoginPage />} />
+            {/* Register path removed since you already have a signup page */}
           </Route>
           
-          {/* Protected routes (with regular layout) */}ected routes (with regular layout) */}
-          <Route element={<PrivateRoute />}><Route element={<PrivateRoute />}>
+          {/* Protected routes (with regular layout) */}
+          <Route element={<PrivateRoute />}>
             <Route element={<Layout />}>
-              <Route path="/dashboard" element={<Dashboard />} />nt={<Dashboard />} />
-              <Route path="/profile" element={<Profile />} />ment={<Profile />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/profile" element={<Profile />} />
               <Route path="/pathways" element={<Pathways />} />
-              <Route path="/lessons/:id" element={<Lesson />} /> />
+              <Route path="/lessons/:id" element={<Lesson />} />
             </Route>
           </Route>
           
-          {/* Admin routes (with admin layout) */}routes (with admin layout) */}
-          <Route element={<PrivateRoute adminOnly={true} />}>lement={<PrivateRoute adminOnly={true} />}>
-            <Route element={<AdminLayout />}>  <Route element={<AdminLayout />}>
-              <Route path="/admin/dashboard" element={<AdminDashboard />} />nt={<AdminDashboard />} />
+          {/* Admin routes (with admin layout) */}
+          <Route element={<PrivateRoute adminOnly={true} />}>
+            <Route element={<AdminLayout />}>
+              <Route path="/admin/dashboard" element={<AdminDashboard />} />
               
               {/* User management */}
               <Route path="/admin/users" element={<AdminUsers />} />
-              <Route path="/admin/users/new" element={<AdminUserForm />} /><Route path="/admin/users/new" element={<AdminUserForm />} />
-              <Route path="/admin/users/:id" element={<AdminUserDetail />} />rs/:id" element={<AdminUserDetail />} />
-              <Route path="/admin/users/:id/edit" element={<AdminUserForm />} />rForm />} />
+              <Route path="/admin/users/new" element={<AdminUserForm />} />
+              <Route path="/admin/users/:id" element={<AdminUserDetail />} />
+              <Route path="/admin/users/:id/edit" element={<AdminUserForm />} />
               
               {/* Pathway management */}
               <Route path="/admin/pathways" element={<AdminPathways />} />
-              <Route path="/admin/pathways/new" element={<AdminPathwayForm />} /><Route path="/admin/pathways/new" element={<AdminPathwayForm />} />
-              <Route path="/admin/pathways/:id" element={<AdminPathwayDetail />} />ys/:id" element={<AdminPathwayDetail />} />
-              <Route path="/admin/pathways/:id/edit" element={<AdminPathwayForm />} />yForm />} />
+              <Route path="/admin/pathways/new" element={<AdminPathwayForm />} />
+              <Route path="/admin/pathways/:id" element={<AdminPathwayDetail />} />
+              <Route path="/admin/pathways/:id/edit" element={<AdminPathwayForm />} />
               
               {/* Lesson management */}
               <Route path="/admin/lessons" element={<AdminLessons />} />
-              <Route path="/admin/lessons/new" element={<AdminLessonForm />} /><Route path="/admin/lessons/new" element={<AdminLessonForm />} />
-              <Route path="/admin/lessons/:id" element={<AdminLessonDetail />} />ns/:id" element={<AdminLessonDetail />} />
-              <Route path="/admin/lessons/:id/edit" element={<AdminLessonForm />} />nForm />} />
+              <Route path="/admin/lessons/new" element={<AdminLessonForm />} />
+              <Route path="/admin/lessons/:id" element={<AdminLessonDetail />} />
+              <Route path="/admin/lessons/:id/edit" element={<AdminLessonForm />} />
             </Route>
           </Route>
         </Routes>
       </Router>
-    </AuthProvider>>
+    </AuthProvider>
   );
 }
 
-export default App;p;
+export default App;
